765e9d384a626f75bcf8031a38b3ec0f132919c9
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-15 00:58:58 +0700

    Update snk.js

--- plugins/snk.js
+++ plugins/snk.js
@@ -1,40 +1,38 @@
 let fetch = require('node-fetch')
-let handler = async(m, { conn }) => {
-    let teks = `
-Kebijakan Privasi, Syarat Ketentuan dan Peraturan Zifabotz!!!
+let handler = async (m, { conn }) => await conn.send2Button(m.chat, `
+╭─────[ *RULES* ]─────✧
+┴
+│¹ Dilarang Hina zifabotz / Owner
+│² Dilarabg Spam Command
+│³ Dilarang Kirim Virtex ke zifabotz
+│⁴ Dilarang Spam Menu Ga Jelas
+│⁵ Dilarang Telp / Vc
+│⁶ Dilarang Culik Bot
+│⁷ Dilarang Promosi
+│⁸ Dilarang Meniru pesan Bot
+│⁹ Bot Tidak Menerima Save Kontak
+│¹⁰ Dilarang Chat Owner Ga Jelas
+│¹¹ No plagiat
+┬
+╰──────────···
 
-Kebijakan Privasi Riel Bot
-1. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ tidak akan merekam data riwayat chat user.
-2. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ tidak akan menyebarkan nomor users.
-3. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ tidak akan menyimpan media yang dikirimkan oleh users.
-4. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ tidak akan menyalah gunakan data data users.
-5. Owner  berhak melihat data riwayat chat users.
-6. Owner  berhak melihat status users.
-7. Owner  dapat melihat riwayat chat, dan media yang dikirimkan users.
+╭─────[ *HUKUM* ]─────✧
+┴
+│ 1 & 11 = Block + banned permanent
+│ 2,4,6 & 8 = Banned sementara
+│ 3 = War
+│ 5 = Block sementara
+│ 9 & 10 = Block permanent
+┬
+╰──────────···
+${wm}
+`.trim(), 'Harap patuhin peraturan tersebut', '⋮☰ Menu', '.menu', 'Owner', '.nowner', m)
 
-Peraturan ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙
-1. Users dilarang menelpon maupun memvideo call nomor bot.
-2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
-3. Users diharap tidak melakukan spam dalam penggunaan bot.
-4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi owner.
-5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
+handler.help = ['rules']
+handler.tags = ['info']
 
-Syarat Ketentuan ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙
-1. Bot akan keluar dari group apabila sudah waktunya keluar.
-2. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
-3. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-4. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-5. ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙ bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+handler.command = /^(rules|peraturan)$/i
 
-- Rozi(owner)
-- ⳹ ❋ཻུ۪۪⸙Zifabotz⳹ ❋ཻུ۪۪⸙
-- ⳹ ❋ཻུ۪۪⸙Zifabotz2⳹ ❋ཻུ۪۪⸙
-- ⳹ ❋ཻུ۪۪⸙Zifabotz3⳹ ❋ཻུ۪۪⸙
-Peraturan: 4 Maret 2022
-`.trim()
-    conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), teks, watermark, 'Menu', '.menu', 'Owner', '.owner', m)
-}
-handler.help = ['peraturan']
-handler.command = /^(snk|syarat|peraturan|rules)$/i
-handler.tags = ['main']
 module.exports = handler
+
+let wm = global.botwm
