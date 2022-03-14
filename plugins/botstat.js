9a7c3e3a73e779a74ca0247322d497e51dbb2c12
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-15 01:13:32 +0700

    Update botstat.js

--- plugins/botstat.js
+++ plugins/botstat.js
@@ -19,18 +19,21 @@ let handler = async (m, { conn }) => {
 ║│➸ *${conn.blocklist.length}* Terblock
 ║│➸ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
 ║│➸ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
-└────
+╰─────────❉
+_____•••••••••
 
-┌─〔 Pengaturan 〕
-├ ${anon ? '✅' : '❌'} *Anon Chat*
-├ ${anticall ? '✅' : '❌'} *Anti Call*
-├ ${antispam ? '✅' : '❌'} *Anti Spam*
-├ ${antitroli ? '✅' : '❌'} *Anti Troli*
-├ ${backup ? '✅' : '❌'} *Auto Backup DB*
-├ ${groupOnly ? '✅' : '❌'} *Mode Grup*
-├ ${jadibot ? '✅' : '❌'} *Jadi Bot*
-├ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
-└────
+╭═══════════════════════
+║╭──❉ 〔 ⳹ ❋ཻུ۪۪PERATURAN ZIFABOTZ⳹ ❋ཻུ۪۪ 〕 ❉────── 
+║│➸ ${anon ? '✅' : '❌'} *Anon Chat*
+║│➸ ${anticall ? '✅' : '❌'} *Anti Call*
+║│➸ ${antispam ? '✅' : '❌'} *Anti Spam*
+║│➸ ${antitroli ? '✅' : '❌'} *Anti Troli*
+║│➸ ${backup ? '✅' : '❌'} *Auto Backup DB*
+║│➸ ${groupOnly ? '✅' : '❌'} *Mode Grup*
+║│➸ ${jadibot ? '✅' : '❌'} *Jadi Bot*
+║│➸ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
+╰─────────❉
+_____•••••••
     `.trim())
 }
 handler.help = ['botstatus']
